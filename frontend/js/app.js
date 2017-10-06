7db31aa55344adb6ce1da194ea0530df8c5f1721
Sava Gerov <sava.jg@gmail.com>
2017-10-06 08:34:08 +0200

    Add react and redux

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,2 +1,15 @@
-alert("This is loaded with webpack through phoenix")
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import reducers from "./reducers";
+import MyStore from "./store";
+
+render(
+          <Provider store={MyStore}>
+                        <App />
+          </Provider>,
+          document.getElementById("root")
+          );
 
