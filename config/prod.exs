8e16aaca5b2a7b3eb85a209002e425695f2f1409
Sava Gerov <sava.jg@gmail.com>
2018-02-01 09:02:08 +0100

    Fix post_start

--- config/prod.exs
+++ config/prod.exs
@@ -44,8 +44,8 @@ config :logger, level: :info
 # We also recommend setting `force_ssl`, ensuring no data is
 # ever sent via http, always redirecting to https:
 #
-#     config :athena, AthenaWeb.Endpoint,
-#       force_ssl: [hsts: true]
+config :athena, AthenaWeb.Endpoint,
+  force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
